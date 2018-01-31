810c7c9f4c68549f6aef74e9ce6b2f1b3e4d606f
xuxiaojun02 <xuxiaojun02@corp.netease.com>
2018-01-31 13:19:29 +0800

    test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)HelloWorld.java, 2018/1/31.
+ * <p/>
+ * Copyright 2018 Netease, Inc. All rights reserved.
+ * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author 徐晓俊(xuxiaojun02 @ corp.netease.com)
+ */
+public class HelloWorld {
+    public static void print(){
+        System.out.println("Hello World");
+    }
+}
