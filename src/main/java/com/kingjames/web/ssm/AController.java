19eeaad1a0a68dd9a226faa2725bf567883431a7
xuxiaojun02 <xuxiaojun02@corp.netease.com>
2018-01-31 15:49:00 +0800

    add jetty support

---
+++ src/main/java/com/kingjames/web/ssm/AController.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)AController.java, 2018/1/31.
+ * <p/>
+ * Copyright 2018 Netease, Inc. All rights reserved.
+ * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.kingjames.web.ssm;
+
+/**
+ * @author 徐晓俊(xuxiaojun02 @ corp.netease.com)
+ */
+public class AController {
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<h2>It is my SSM demo project!</h2>
 </body>
 </html>
